fb655fcd2355b5b774da542ef61dc3645c43b54a
elolelo <lelonompumelelo@gmail.com>
2020-12-22 01:33:55 +0200

    Fixed a small bug

--- src/components/Publications.js
+++ src/components/Publications.js
@@ -16,7 +16,7 @@ class Publications extends Component {
                 <div className="card-body ">
                   <div className="legend">
                   <i className="fa fa-file-text-o"></i> Blog post 1 by Data Science for Social Impact
-                    <h4 ></h4><a href="https://dsfsi.github.io/blog/mapping-healthsystem/" target="blank">Mapping the South African Health Facility landscape in response to Covid-19</a>
+                    <h4><a href="https://dsfsi.github.io/blog/mapping-healthsystem/" target="blank">Mapping the South African Health Facility landscape in response to Covid-19</a></h4>
                                        
                                        
                                     </div>
@@ -24,7 +24,7 @@ class Publications extends Component {
                   <hr />
                   <div className="legend">
                   <i className="fa fa-file-text-o"></i> Blog post 2 by Afrimapr
-                    <h4 ></h4><a href="https://afrimapr.github.io/afrimapr.website/blog/2020/visualisation-to-improve-data-quality/" target="blank">How an R-map visualization has helped to improve the state of the SA health facilities data in the covid19za repository</a>
+                    <h4><a href="https://afrimapr.github.io/afrimapr.website/blog/2020/visualisation-to-improve-data-quality/" target="blank">How an R-map visualization has helped to improve the state of the SA health facilities data in the covid19za repository</a></h4>
                                        
                                        
                                     </div>
@@ -36,8 +36,8 @@ class Publications extends Component {
             <div className="col-md-8">
               <div className="card">
                 <div className="card-header ">
-                  <h4 className="card-title">KDD humanitarian workshop paper</h4>
-                  <p className="card-category"><a href="https://kdd-humanitarian-mapping.herokuapp.com/" target="blank">About the workshop</a></p>
+                  <h3 className="card-title">KDD humanitarian workshop paper</h3>
+                  <h5 className="card-category"><a href="https://kdd-humanitarian-mapping.herokuapp.com/" target="blank">About the workshop</a></h5>
                 </div>
                 <Iframe url="https://arxiv.org/pdf/2006.15216.pdf"
                   width="100%"
